77da081115269fcb128defaee54c2265c4c03e53
Young.Geo <anxan@Young.Geo>
2018-01-17 20:26:37 +0800

    m

--- src/node_db/src/main.cpp
+++ src/node_db/src/main.cpp
@@ -1,8 +1,18 @@
 #include "work.h"
+#include "rclcpp/rclcpp.hpp"
 
 
-int main()
+int main(int argc, const char **argv)
 {
+    /*
+    rclcpp::init(argc, (char **)argv);
+    auto node = rclcpp::Node::make_shared("node_db");
+
+    while (rclcpp::ok())
+    {
+        //sleep();
+        rclcpp::spin_some(node);
+    }*/
     /////
     ////
     gWORK->Init();
